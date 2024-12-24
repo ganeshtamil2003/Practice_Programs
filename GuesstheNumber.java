8b755b04c01a0251b66331c6aff1d00136c5958c
ganeshtamil2003 <ganesh28062003@gmail.com>
2024-12-24 14:46:41 +0530

    code added

--- GuesstheNumber.java
+++ GuesstheNumber.java
@@ -1,31 +1,41 @@
 import java.util.*;
-class GuesstheNumber{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int guess = (int) (Math.random()*100);
-        int times=10;
-        System.out.println(" **Guess the Number within 10 Guess** ");
-        System.out.println("Enter the Number between (0 to 101) : ");
-        while(times > 0){
-            int number = sc.nextInt();
-            if(number == guess){
-            System.out.println("You've Guessed Right");
-            break;
-            }
-            else if(number > guess){
-                System.out.println("You've Entered Greater than Answer");
-                times--;
-                System.out.println( times + " Guess left" );
-                
-            }
-            else if(number < guess){
-                System.out.println("You've Entered Lesser than Answer");
-                times--;
-                System.out.println( times + " Guess left" );
 
+class GuesstheNumber {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int guess = (int) (Math.random() * 101); // Random number between 0 and 100 (inclusive)
+        int attempts = 5;
+
+        System.out.println("** Guess the Number within 5 Guesses **");
+        System.out.println("Enter a number between 0 and 100:");
+
+        while (attempts > 0) {
+            int number = sc.nextInt();
+
+            // Validate input range
+            if (number < 0 || number > 100) {
+                System.out.println("Invalid input! Please enter a number between 0 and 100.");
+                continue;
+            }
+
+            if (number == guess) {
+                System.out.println("Congratulations! You've guessed the number correctly.");
+                break;
+            } else if (number > guess) {
+                System.out.println("You've entered a number greater than the answer.");
+            } else {
+                System.out.println("You've entered a number lesser than the answer.");
+            }
+
+            attempts--;
+            if (attempts > 0) {
+                System.out.println(attempts + " guess(es) left.");
+            } else {
+                System.out.println("Better luck next time!");
+                System.out.println("The correct number was: " + guess);
             }
         }
-        //System.out.println(guess);
 
+        sc.close(); // Close the scanner
     }
 }
